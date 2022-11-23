77b575013f8e53011e1cfeb365d3ea390477db22
oneyellowflower <apdl5929@gmail.com>
2022-11-23 08:11:02 +0000

    연습

--- portfolio.js
+++ portfolio.js
@@ -1,31 +1,32 @@
 var swiper1 = new Swiper(".page", {
-    direction: "vertical",
-    slidesPerView: 1,
-    mousewheel: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    keyboard: {
-        enabled: true,
-      },
-  });
+  direction: "vertical",
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
 
-  $(".tab").click(function () {
-    var tab1 = $(this).index();
-    $(this).addClass("on").siblings().removeClass("on");
-    swiper1.slideTo(eq(tab1), 300);
-  });
+$(".tab").click(function () {
+  var tab1 = $(this).index();
+  $(this).addClass("on").siblings().removeClass("on");
+  swiper1.slideTo(eq(tab1), 300);
+});
 
-  document.querySelector('#contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // submit이벤트 막기
-    const fromName = document.querySelector('input[name="from_name"]').value; // 전송자 이름 추출
-    emailjs.init("T2lq3B83sC2b9TJvY"); // API keys
-    emailjs.sendForm('service_flower', 'template_sp7m06w', this)
-        .then(function() {
-            alert(`${fromName}님, 메일 전송 완료 되었습니다 :)`)
-        }, function(error) {
-            alert(`${fromName}님, 메일 전송이 실패했습니다 :(`)
-            console.log('전송실패', error);
-        });
+document.querySelector('#contactForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // submit이벤트 막기
+  const fromName = document.querySelector('input[name="from_name"]').value; // 전송자 이름 추출
+
+  emailjs.init("kzbfIgjDLXOPAFLOI"); // API keys
+  emailjs.sendForm('service_j93d0xs', 'template_mlgda0f', this)
+    .then(function () {
+      alert(`${fromName}님, 메일 전송 완료 되었습니다 :)`);
+    }, function (error) {
+      alert(`${fromName}님, 메일 전송이 실패했습니다 :(`);
+      console.log('전송실패', error);
+    });
 });
